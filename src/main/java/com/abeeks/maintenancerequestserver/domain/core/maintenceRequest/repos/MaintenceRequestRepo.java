05955548d67ef99b102117bf42700870d60e3d07
Student10 <student10@Codes-MacBook-Air-85.local>
2023-12-21 07:23:59 -0500

    2nd commit

--- src/main/java/com/abeeks/maintenancerequestserver/domain/core/maintenceRequest/repos/MaintenceRequestRepo.java
+++ src/main/java/com/abeeks/maintenancerequestserver/domain/core/maintenceRequest/repos/MaintenceRequestRepo.java
@@ -1,4 +1,10 @@
 package com.abeeks.maintenancerequestserver.domain.core.maintenceRequest.repos;
 
-public class MaintenceRequestRepo {
+import com.abeeks.maintenancerequestserver.domain.core.maintenceRequest.models.MaintenceRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MaintenceRequestRepo extends JpaRepository<MaintenceRequest, Long> {
+    Optional<MaintenceRequest> findByEmail(String email);
 }
--- src/test/java/com/abeeks/maintenancerequestserver/MaintenanceRequestServerApplicationTests.java
+++ src/test/java/com/abeeks/maintenancerequestserver/MaintenanceRequestServerApplicationTests.java
@@ -1,5 +1,7 @@
 package com.abeeks.maintenancerequestserver;
 
+import com.abeeks.maintenancerequestserver.domain.core.maintenceRequest.models.MaintenceRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 class MaintenanceRequestServerApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	public void constructorTest01() {
+		MaintenceRequest maintenceRequest = new MaintenceRequest("Spongebob", "Squarepants", "sponge@bikinibottom.com", "Apt 1", "Plumbing issue");
+		maintenceRequest.setId(2l);
 
+		String expected = "2 Spongebob Squarepants sponge@bikinibottom.com Apt 1 Plumbing issue";
+		String actual = maintenceRequest.toString();
+
+		Assertions.assertEquals(expected, actual);
+	}
 }
